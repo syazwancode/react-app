0e0af0ffc09d814ff3a34ca4daa633ad9e3093c4
Mohamad Syazwan Ahmad Zubir <syazwan@invokeisdata.com>
2020-08-28 14:23:48 +0800

    Updates

---
+++ precache-manifest.238c2f925a2e24d7927424ffd3d8cebc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431ae35bcf84f6ef403b8c454cb1bd86",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "3396848d9487e8ef1fb3",
+    "url": "/react-app/static/css/2.4df217b3.chunk.css"
+  },
+  {
+    "revision": "d62baf12ffb8d4e26eea",
+    "url": "/react-app/static/css/main.1b9f3207.chunk.css"
+  },
+  {
+    "revision": "3396848d9487e8ef1fb3",
+    "url": "/react-app/static/js/2.61278c71.chunk.js"
+  },
+  {
+    "revision": "d62baf12ffb8d4e26eea",
+    "url": "/react-app/static/js/main.bec30470.chunk.js"
+  },
+  {
+    "revision": "f38ad66aa2f5766f8023",
+    "url": "/react-app/static/js/runtime-main.2ef98780.js"
+  },
+  {
+    "revision": "4fd94c63db5327e5c0b336c1c958e269",
+    "url": "/react-app/static/media/lazada_banner.4fd94c63.png"
+  },
+  {
+    "revision": "9297a28ea405959b1f0c145087e4af8e",
+    "url": "/react-app/static/media/lazada_square.9297a28e.png"
+  },
+  {
+    "revision": "6bfd1f8ef0eea2c5dcc43115d8607687",
+    "url": "/react-app/static/media/shopee_banner.6bfd1f8e.png"
+  },
+  {
+    "revision": "f57b6a07d7e7e7a97d80381d182ec6ef",
+    "url": "/react-app/static/media/shopee_square.f57b6a07.png"
+  }
+]);
